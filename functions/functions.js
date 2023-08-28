a2271e30a4ade12b33bfe40e4f7374612863d5c3
Mb4y4 <d.sommy@gmail.com>
2023-08-28 21:40:58 +0300

    functions

--- functions/functions.js
+++ functions/functions.js
@@ -10,17 +10,19 @@
 //     }
 //     sayHello('Denis');
 
+
 // const addition = function(a, b) {
 //     return a + b;
 // }
 
 // console.log(`${addition(20 , 3)}`);
 
-function factorial(n){
-    if (n === 0 || n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-        }
-}
-console.log(`${factorial(5)}`);
+
+// function factorial(n){
+//     if (n === 0 || n === 1) {
+//         return 1;
+//     } else {
+//         return n * factorial(n - 1);
+//         }
+// }
+// console.log(`${factorial(5)}`);
